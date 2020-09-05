0c12af956de5401fa695092f2fff5bfb05ae2603
Shreeji <55141517+Shreejichandra@users.noreply.github.com>
2020-09-05 18:16:35 +0530

    Create 1305_all_elements_in_two_binary_search_trees.py

---
+++ 1305_all_elements_in_two_binary_search_trees.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getAllElements(self, root1: TreeNode, root2: TreeNode) -> List[int]:
+        l1 = []
+        l2 = []
+        self.inorder(root1, l1)
+        self.inorder(root2, l2)
+        return sorted(l1 + l2)
+        
+    def inorder(self, root, l):
+        if not root:
+            return
+        self.inorder(root.left, l)
+        l.append(root.val)
+        self.inorder(root.right, l)
