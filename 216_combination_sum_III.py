e496a2e8ae29bef1034bd3a6fa17865e09674798
Shreeji <55141517+Shreejichandra@users.noreply.github.com>
2020-09-12 17:31:56 +0530

    Update 216_combination_sum_III.py

--- 216_combination_sum_III.py
+++ 216_combination_sum_III.py
@@ -1,3 +1,5 @@
+#Find all possible combinations of k numbers that add up to a number n, given that only numbers from 1 to 9 can be used and each combination should be a unique set of numbers.
+
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
